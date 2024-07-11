b3001bb037e654f65521b77eed9c3e889d827c99
mimhle <40392166+mimhle@users.noreply.github.com>
2024-07-11 10:37:38 +0700

    Added banner placeholder

--- app/page.js
+++ app/page.js
@@ -3,6 +3,15 @@
 import ItemList from "@/app/ItemList";
 import { getCatalogs } from "@/lib/firebase_server";
 import { useEffect, useState } from "react";
+import { Carousel } from "antd";
+
+const contentStyle = {
+    height: "30rem",
+    color: "#fff",
+    lineHeight: "160px",
+    textAlign: "center",
+    background: "#364d79",
+};
 
 export default function Home() {
     const [catalogs, setCatalogs] = useState({});
@@ -15,6 +24,20 @@ export default function Home() {
 
     return (
         <div>
+            <Carousel autoplay>
+                <div>
+                    <h3 style={contentStyle}>1</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>2</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>3</h3>
+                </div>
+                <div>
+                    <h3 style={contentStyle}>4</h3>
+                </div>
+            </Carousel>
             {Object.keys(catalogs).map((key) => {
                 return <div key={key} className="my-2">
                     <ItemList key={key} catalog={key} extra={`/${catalogs[key].name.toLowerCase()}`}
