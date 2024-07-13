8e4f7affc9081f4fe96fe91c3263c4d5a28fcfb6
NGUYEN THINH <119058000+hieu79115@users.noreply.github.com>
2024-07-13 12:52:51 +0700

    push

---
+++ components/paymentPage/itemInputText.js
@@ -0,0 +1,29 @@
+'use client';
+export default function ItemInputText({
+    forPropertyLabel,
+    labelValue,
+    idInputValue,
+    widthInputValue,
+    placeholderInputValue,
+    inputValue = '',
+    readOnly = false,
+}) {
+    const defaultStyle = 'border border-gray-300 rounded px-3 py-2 text-sm';
+    const style = defaultStyle + ' ' + widthInputValue;
+    return (
+        <div className="flex flex-col space-y-1 items-start w-full sm:items-start">
+            <label  className="text-base text-gray-500">
+                {labelValue}
+            </label>
+            <input
+                className={style}
+                type="text"
+                id={idInputValue}
+                style={{ width: widthInputValue }}
+                placeholder={placeholderInputValue}
+                {...(inputValue !== '' ? { value: inputValue } : {})}
+                readOnly={readOnly}
+            />
+        </div>
+    );
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // supported url for remote image
+    images: {
+        domains: ['media3.coolmate.me'],
+    },
+};
 
 export default nextConfig;
