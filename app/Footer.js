5593822f798e6ca9e9202b222403404b6ccc559a
TuanAnh <lngtuananh09@gmail.com>
2024-07-23 16:17:15 +0700

    move subcribe to Footer

--- app/Footer.js
+++ app/Footer.js
@@ -68,6 +68,49 @@ const Footer = () => {
                             <p>Email: K48@Shop.hcmue.edu.vn</p>
                             <p>Phone: 0909 123 456</p>
                         </div>
+                        <div id="amc_embed_shell">
+                                    <link href="//cdn-images.mailchimp.com/embedcode/classic-061523.css"
+                                          rel="stylesheet" type="text/css"/>
+                                    <style type="text/css">
+                                        {`
+                                            #mc_embed_signup{background:#001529; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                                            /* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+                                               We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+                                        `}
+                                    </style>
+                                    <div id="mc_embed_signup">
+                                        <form
+                                            action="https://zohomail.us17.list-manage.com/subscribe/post?u=16511ef613aa60fd4375c250b&amp;id=02fd0253cd&amp;f_id=0087c2e1f0"
+                                            method="post" id="mc-embedded-subscribe-form"
+                                            name="mc-embedded-subscribe-form" className="validate" target="_blank">
+                                            <div id="mc_embed_signup_scroll">
+                                                
+                                                <div className="mc-field-group">
+                                                    <label htmlFor="mce-EMAIL">Your Email Address <span
+                                                        className="asterisk">*</span></label>
+                                                    <input type="email" name="EMAIL" className="required email bg-[#001529]"
+                                                           id="mce-EMAIL" defaultValue={""}/>
+                                                </div>
+                                                
+                                                <div id="mce-responses" className="clear foot">
+                                                    <div className="response" id="mce-error-response"
+                                                         style={{ display: "none" }}></div>
+                                                    <div className="response" id="mce-success-response"
+                                                         style={{ display: "none" }}></div>
+                                                </div>
+                                                <div style={{ position: "absolute", left: "-5000px" }}
+                                                     aria-hidden="true">
+                                                    <input type="text" name="b_16511ef613aa60fd4375c250b_02fd0253cd"
+                                                           tabIndex="-1" value=""/>
+                                                </div>
+                                                <div className="clear">
+                                                    <input type="submit" value="Subscribe" name="subscribe"
+                                                           id="mc-embedded-subscribe" className="button"/>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
                     </div>
 
                     <div>
