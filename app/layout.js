206b75bd1e4e41e515a5f65c0947d45d9983a43a
Binh <binhwork245@gmail.com>
2024-08-11 20:34:31 +0700

    change analytics to below body

--- app/layout.js
+++ app/layout.js
@@ -25,9 +25,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <head>
-                <GoogleAnalytics />
-            </head>
             <body className={`${inter.className}`}>
                 <AntdRegistry>
                     <MainLayout>
